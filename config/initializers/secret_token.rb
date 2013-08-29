08d2c6a0ccb8725d31a09183d508ddd3e5725b25
maasdesigner <maasgultekin@gmail.com>
2013-08-29 10:54:21 +0300

    First gallery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Galery::Application.config.secret_token = 'ba5693e87259fa5f52ab9f10a0cdb074f905fff21a6c0283fc2059ce9b8fa51fc8e6ae0c48abcd5e0002fb7276f7e3dd9188f216a84b6c3d43748bd90465cea7'
